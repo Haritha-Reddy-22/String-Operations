32573655a312836ed20c8601180b8c94e1c00b8d
Haritha Reddy <kambhamharitha49@gmail.com>
2025-01-31 04:59:26 +0530

    Add files via upload

---
+++ StringOperations.java
@@ -0,0 +1,9 @@
+public class StringOperations {
+    public static void main(String[] args) {
+        String str = "hello";
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Length: " + str.length());
+        System.out.println("Replaced: " + str.replace('e', 'a'));
+        System.out.println("Substring: " + str.substring(1, 4));
+    }
+}
